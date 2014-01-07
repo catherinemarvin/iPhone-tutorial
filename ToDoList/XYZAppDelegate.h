0cccebbbbf38b1bc9baaae47bac2f63501208816
Kevin Hwang <k3vinhwang@gmail.com>
2014-01-07 15:28:55 -0800

    Initial Commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Kevin Hwang on 1/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
